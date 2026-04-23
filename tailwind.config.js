90d61992ffa5fed664835cfd8a722b80d44283c9
Tamim Shadman <tamimshadman2000@gmail.com>
2026-04-23 14:06:31 +0600

    console fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        fruit: {
+          red: "#f85559",
+          peach: "#ffd8a8",
+          cream: "#fff8ed",
+          leaf: "#1f8f5f",
+          ink: "#172233"
+        }
+      },
+      fontFamily: {
+        heading: ["Fraunces", "serif"],
+        body: ["Manrope", "sans-serif"]
+      }
+    }
+  },
+  plugins: [require("daisyui")],
+}
